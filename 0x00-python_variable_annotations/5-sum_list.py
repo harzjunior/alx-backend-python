80bc25a6741e2f749551bacd3868f63ac1ad34ed
Haruna Bah <harunjibs@gmail.com>
2024-03-14 23:22:05 +0100

    5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Task 5's module.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Computes sum of a list of floating-ptn nums.
+    '''
+    return float(sum(input_list))
