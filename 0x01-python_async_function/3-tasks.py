47ad6579d18a7bf59f9d547cadeda09629c43dd3
Haruna Bah <harunjibs@gmail.com>
2024-03-18 09:21:32 +0100

    3-tasks.py

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Task 3's module.
 '''
 import asyncio
-import time
 
 
-wait_n = __import__('1-concurrent_coroutines').wait_n
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def measure_time(n: int, max_delay: int) -> float:
-    '''Computes the ave runtime of wait_n.
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an async task for wait_random.
     '''
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    return (time.time() - start_time) / n
+    return asyncio.create_task(wait_random(max_delay))
